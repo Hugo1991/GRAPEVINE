d439437cf16cf4ff11b6f4a2e07cdfc05c7f7aae
Hugo1991 <hugofernandezvisier@gmail.com>
2015-01-16 01:07:14 +0100

    creacion de clases//falta atributos y metodos

---
+++ Grapevine/src/grapevine/Cita.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grapevine;
+
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Cita {
+    private GregorianCalendar fecha;
+    private GregorianCalendar hora;
+}
